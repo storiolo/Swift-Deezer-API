89bdd8fb7d9178f165afefb0bb70055fed63925c
Nicolas Storiolo <nicosto40@gmail.com>
2023-10-30 18:09:10 +0100

    '

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "DeezerAPI",
+    name: "Swift-Deezer-API",
     platforms: [
         .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DeezerAPI",
-            targets: ["DeezerAPI"]),
+            name: "Swift-Deezer-API",
+            targets: ["Swift-Deezer-API"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,7 +23,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "DeezerAPI",
+            name: "Swift-Deezer-API",
             dependencies: ["Alamofire"]),
     ]
 )
