cd56c3e85d57a913e70eec9ed5a303e89e7b9967
Nicolas Storiolo <nicosto40@gmail.com>
2023-11-02 13:51:52 +0100

    '

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "DeezerAPI",
+            name: "Swift-Deezer-API",
             targets: ["DeezerAPI"]),
     ],
     dependencies: [
